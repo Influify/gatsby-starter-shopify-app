624c5c5645c5f3e5cbfabf17e9d59a2df0d36a50
Influify <influify@gmail.com>
2020-04-15 09:03:25 -0400

    Update index.js

--- functions/config/index.js
+++ functions/config/index.js
@@ -1,4 +1,4 @@
-exports.APP_PLAN_NAME = '(test2) Starter Plan';
+exports.APP_PLAN_NAME = 'Basic Influify';
 exports.ACTIVATE_CHARGE_ROUTE = '/api/activate-charge';
-exports.APP_PLAN_PRICE = 24.99;
+exports.APP_PLAN_PRICE = 5.99;
 exports.APP_PLAN_TRIAL_DURATION = 7;
